4a4cb744c34aac777cabb5bd09bfaabc34dfd537
Os1o <seblverde19@outlook.com>
2025-09-05 16:27:59 -0600

    Update

--- js/script.js
+++ js/script.js
@@ -2434,5 +2434,3 @@ function showNotification(message, type = 'info') {
 // ===================================
 
 console.log('Script.js cargado completamente - ' + APP_CONFIG.name);
-console.log('Tipo de solicitud recibido:', tipo);
-console.log('Recurso actual:', recursoActual);
